192656e09349b31f4e98cdeb74a7dc7d487e6240
MattBrozusky <matthew.brozusky@gmail.com>
2018-11-20 10:30:54 -0600

    Suppresed more warings

--- src/Author.java
+++ src/Author.java
@@ -3,7 +3,7 @@
  */
 import java.util.ArrayList;
 import java.util.List;
-@SuppressWarnings("all")
+@SuppressWarnings("unchecked")
 public class Author extends Person {
     private List books;
 
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 public class Main {
+    @SuppressWarnings("deprecated")
     public static void main(String[] args) {
         Author author = new Author("Sandi", "Metz");
 
